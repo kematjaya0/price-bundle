110a61e8aba9f70a6f91c6c0b3bdd518cd84986a
Nur Hidayatullah <>
2023-05-30 11:58:17 +0700

    add configuration allow negative

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,29 +17,30 @@ use Symfony\Component\Config\Definition\Builder\NodeBuilder;
  */
 class Configuration implements ConfigurationInterface
 {
-    
-    public function getConfigTreeBuilder(): TreeBuilder 
+
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('price');
         $rootNode = $treeBuilder->getRootNode();
-        
+
         $this->addCurrencyConfiguration($rootNode->children());
-        
+
         return $treeBuilder;
     }
-    
+
     protected function addCurrencyConfiguration(NodeBuilder $node):void
     {
         $node
             ->arrayNode('currency')
-                ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('code')->defaultValue('IDR')->end()
-                        ->integerNode('cent_limit')->defaultValue(0)->end()
-                        ->scalarNode('cent_point')->defaultValue('.')->end()
-                        ->scalarNode('thousand_point')->defaultValue(',')->end()
-                    ->end()
-                ->end()
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('code')->defaultValue('IDR')->end()
+            ->integerNode('cent_limit')->defaultValue(0)->end()
+            ->scalarNode('cent_point')->defaultValue('.')->end()
+            ->scalarNode('thousand_point')->defaultValue(',')->end()
+            ->booleanNode("allow_negative")->defaultValue(true)->end()
+            ->end()
+            ->end()
             ->end();
     }
 }
--- src/Type/PriceType.php
+++ src/Type/PriceType.php
@@ -3,6 +3,7 @@
 namespace Kematjaya\PriceBundle\Type;
 
 use Kematjaya\PriceBundle\Lib\CurrencyFormatInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\CallbackTransformer;
@@ -13,23 +14,23 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 /**
  * @author Nur Hidayatullah <kematjaya0@gmail.com>
  */
-class PriceType extends MoneyType 
-{   
-    /**
-     * 
-     * @var CurrencyFormatInterface
-     */
-    private $currencyFormat;
+class PriceType extends MoneyType
+{
+    private CurrencyFormatInterface $currencyFormat;
 
-    public function __construct(CurrencyFormatInterface $currencyFormat)
+    private array $configs;
+
+    public function __construct(CurrencyFormatInterface $currencyFormat, ParameterBagInterface $bag)
     {
+        $this->configs = $bag->get("price")["currency"];
+
         $this->currencyFormat = $currencyFormat;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         parent::configureOptions($resolver);
-        
+
         $resolver->setDefaults([
             'invalid_message' => 'The selected issue does not exist',
             'currency' => $this->currencyFormat->getCurrencySymbol(),
@@ -40,49 +41,50 @@ class PriceType extends MoneyType
             'scale' => (int)$this->currencyFormat->getCentLimit()
         ]);
     }
-    
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->addModelTransformer(
             new CallbackTransformer(
                 function ($value) use ($options) {
-                
+
                     if (0 == $options['scale']) {
-                        
+
                         return $value;
                     }
-                    
+
                     $values = explode(".", $value);
                     $values[0] = 0 !== strlen($values[0]) ? $values[0]: 0;
                     $values[1] = isset($values[1]) ? $values[1]: 0;
                     for ($i = strlen($values[1]); $i < $options['scale']; $i++) {
                         $values[1] .= '0';
                     }
-                    
+
                     return implode(".", $values);
                 }, function (?string $value) use ($options) {
-                    if (null === $value) {
-                        
-                        return 0;
-                    }
-                    
-                    return $this->currencyFormat->priceToFloat($value, $options['scale']);
+                if (null === $value) {
+
+                    return 0;
                 }
+
+                return $this->currencyFormat->priceToFloat($value, $options['scale']);
+            }
             )
         );
     }
-    
+
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         parent::buildView($view, $form, $options);
-        
+
         if (false === strpos($view->vars['money_pattern'], $this->currencyFormat->getCurrencySymbol())) {
             $view->vars['money_pattern'] = sprintf("%s %s", $this->currencyFormat->getCurrencySymbol(), $view->vars['money_pattern']);
         }
-        
+
         $view->vars["attr"] = !empty($view->vars['attr']) ? $view->vars['attr'] : ["style" => 'text-align: right'];
         $view->vars['prefix'] = "";
         $view->vars['suffix'] = "";
+        $view->vars['allow_negative'] = $this->configs["allow_negative"];
         $view->vars['cents_separator'] = $options['cents-separator'];
         $view->vars['thousands_separator'] = $options['thousands-separator'];
         $view->vars['scale'] = isset($options['scale']) ? $options['scale'] : 0;
