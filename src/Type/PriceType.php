94f0bec73783f0db87a26ce27b2c8940fd38cb0d
Nur Hidayatullah <>
2024-12-19 03:33:58 +0700

    set currency symbol form

--- src/Type/PriceType.php
+++ src/Type/PriceType.php
@@ -67,7 +67,7 @@ class PriceType extends MoneyType
                     return 0;
                 }
 
-                return $this->currencyFormat->priceToFloat($value, $options['scale']);
+                return $this->currencyFormat->priceToFloat($value, $options['currency']);
             }
             )
         );
